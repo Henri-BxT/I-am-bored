df8ca4d840f3fa7d76d1a31ffac37efdf42c0c2d
SoluX1 <33317508+SoluX1@users.noreply.github.com>
2018-05-24 14:23:44 +0200

    debug

--- view/view_home_page.php
+++ view/view_home_page.php
@@ -7,13 +7,13 @@
 <body>
 
 <div align="right">
-<form action="../view/view_registration.html" method="POST">
+<form action="../view/view_sign_in.html" method="POST">
 <input align="right" type="submit" name="Sign In" value="Sign In">
 </form>
 </div>
 
 <div align="right">
-<form action="../view/view_sign_in.html" method="POST">
+<form action="../view/view_registration.html" method="POST">
 <input align="right" type="submit" name="Sign Up" value="Sign Up">
 </form>
 <div>
