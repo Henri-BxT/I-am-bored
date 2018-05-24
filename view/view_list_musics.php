acad104369c7974b05eb012abc8d98f5dd8de913
Joran-FreaZy <38242052+Joran-FreaZy@users.noreply.github.com>
2018-05-24 16:07:04 +0200

    Ajout de navigations entre les pages

---
+++ view/view_list_musics.php
@@ -0,0 +1,20 @@
+<?php
+$login = $_SESSION['id'];
+?>
+
+<!DOCTYPE HTML>
+
+<html>
+<head>
+		<title>List Musics</title>
+</head>
+
+<body>
+<div align="right">
+Welcome <?php print("$login"); ?></a>
+<a href="../controller/controller_profil.php">Return</a>
+<a href="controller_logout.php">Logout</a>
+</div>
+
+</body>
+</html>
