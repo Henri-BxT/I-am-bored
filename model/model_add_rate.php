d8e8dc5cd671dc9471297e6cd538d15e014643c7
Joran-FreaZy <38242052+Joran-FreaZy@users.noreply.github.com>
2018-05-25 13:53:01 +0200

    Début du model add rate

---
+++ model/model_add_rate.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$user = "root";
+$bdd = "im_bored";
+$password = "";
+
+$db_connexion = mysqli_connect($host, $user, $password, $bdd) or die ("Error can't connect to the database");
+
+?>
