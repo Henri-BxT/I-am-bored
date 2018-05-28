42d17c3b3ed11754aa6c3cd2ca7c31f6de5758cb
SoluX1 <33317508+SoluX1@users.noreply.github.com>
2018-05-28 16:17:09 +0200

    correct merge problem

--- controller/controller_profile_movies.php
+++ controller/controller_profile_movies.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 require_once("../view/view_profile_list.html");
-require("functions/affichage.php");
 $db_connect = mysqli_connect("localhost", "root", "", "im_bored") or die ("Error can't connect to the database");
 if(!empty($_REQUEST['movie_search'])){
 	$search = $_REQUEST['movie_search'];
@@ -16,6 +15,7 @@ if(!empty($_REQUEST['movie_search'])){
 			foreach($array as $movie){
 				echo "<td>".$movie."</td>";
 			}
+		}
 		}else{
 			print("Not found");
 		}
@@ -25,7 +25,7 @@ if(!empty($_REQUEST['movie_search'])){
 		$table = "MOVIES";
 		$column = "id_movie";
 		$id = $_SESSION['id'];
-		$liste = mysqli_fetch_all(profile_list($db_connect,$select,$table,$column,$id), MYSQLI_NUM) or die ("Error can't connect to the database");
+		$liste = mysqli_fetch_all(profile_list($db_connect,$select,$table,$column,$id), MYSQLI_NUM);
 		if(!empty($liste)){
 			echo "<tr>";
 			foreach($liste as $array){
@@ -33,33 +33,11 @@ if(!empty($_REQUEST['movie_search'])){
 					echo "<td>".$movie."</td>";
 				}
 			echo "</tr>";
-			};
+			}
 		}else{
 			echo "Your list is empty.<BR>";
 		}
-			echo "</tr>";
-		}
-	}else{
-		print("Not found");
 	}
-}else {
-	$select = "title";
-	$table = "MOVIES";
-	$column = "id_movie";
-	$id = $_SESSION['id'];	
-	require("../model/model_profile_list.php");
-	$liste = mysqli_fetch_all(profile_list($db_connect,$select,$table,$column,$id), MYSQLI_NUM);
-	if(!empty($liste)){
-		echo "<tr>";
-		foreach($liste as $array){
-			foreach($array as $movie){
-				echo "<td>".$movie."</td></tr>";
-			}
-		};
-	}else{
-		echo "Your list is empty.<BR>";
-	}
-}
 mysqli_close($db_connect);
-echo "</TABLE></CENTER><BR><a href='controller_profil.php'>Retour</a></BODY></HTML>"
+echo "</TABLE></CENTER><BR><a href='controller_profil.php'>Retour</a></BODY></HTML>";
 ?>
