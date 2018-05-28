918443741c40eb2d23dfb7e05f3a50e56e33deb0
Joran-FreaZy <38242052+Joran-FreaZy@users.noreply.github.com>
2018-05-28 10:06:16 +0200

    Amélioration de la requête profil_search

--- model/model_profile_search.php
+++ model/model_profile_search.php
@@ -2,7 +2,7 @@
 
 $login = $_SESSION['id'];
 
-function movie_profil_search($SESSION, $POST){
+function movie_profil_search($login, $search){
 
 	$host = "localhost";
 	$user = "root";
@@ -18,7 +18,13 @@ function movie_profil_search($SESSION, $POST){
 
 	mysqli_free_result($REQ);
 	
-	$SQL = 'SELECT listed_movies.id_movie, movies.title, movies.image, listed_movies.grade FROM listed_movies JOIN members ON listed_movies.id_member = members.id_member JOIN movies ON listed_movies.id_movie = movies.id_movie WHERE movies.title AND id_member = "'.$tmp[0].'" LIKE = "'.$search.'"%';
+	$SQL = 'SELECT listed_movies.id_movie, movies.title, movies.image, listed_movies.grade 
+	FROM listed_movies 
+	JOIN members ON listed_movies.id_member = members.id_member 
+	JOIN movies ON listed_movies.id_movie = movies.id_movie 
+	WHERE movies.title LIKE "'.$search'"% 
+	AND members.id_member = "'.$tmp[0].'"';
+	
 	$REQ = mysqli_query($db_connexion, $SQL);
 
 	$movies = mysqli_fetch_array($REQ, MYSQLI_NUM);
@@ -27,7 +33,7 @@ function movie_profil_search($SESSION, $POST){
 	return $movies;
 }
 
-function music_profil_search($SESSION, $POST){
+function music_profil_search($login, $search){
 
 	$host = "localhost";
 	$user = "root";
@@ -43,7 +49,13 @@ function music_profil_search($SESSION, $POST){
 
 	mysqli_free_result($REQ);
 	
-	$SQL = 'SELECT listed_musics.id_music, musics.title, musics.image, listed_musics.grade FROM listed_musics JOIN members ON listed_musics.id_member = members.id_member JOIN musics ON listed_musics.id_music = musics.id_music WHERE musics.title AND id_member = "'.$tmp[0].'" LIKE = "'.$search.'"%';
+	$SQL = 'SELECT listed_musics.id_music, musics.title, musics.image, listed_musics.grade 
+			FROM listed_musics 
+			JOIN members ON listed_musics.id_member = members.id_member 
+			JOIN musics ON listed_musics.id_music = musics.id_music 
+			WHERE musics.title LIKE "'.$search'"% 
+			AND members.id_member = "'.$tmp[0].'"';
+			
 	$REQ = mysqli_query($db_connexion, $SQL);
 
 	$musics = mysqli_fetch_array($REQ, MYSQLI_NUM);
