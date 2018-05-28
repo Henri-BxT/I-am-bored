0e532bd6a92d254d6a85d66f02bdc1a6cdc282ae
Joran-FreaZy <38242052+Joran-FreaZy@users.noreply.github.com>
2018-05-28 16:08:45 +0200

    changes

--- controller/controller_profile_movies.php
+++ controller/controller_profile_movies.php
@@ -3,39 +3,18 @@ session_start();
 require_once("../view/view_profile_list.html");
 require("functions/affichage.php");
 $db_connect = mysqli_connect("localhost", "root", "", "im_bored") or die ("Error can't connect to the database");
-
-if(isset($_REQUEST['movie_search'])){
-	if(empty($_REQUEST['movie_search']) || $_REQUEST['movie_search'] == " "){	
-		require("../model/model_profile_list.php");
-		$select = "title";
-		$table = "MOVIES";
-		$column = "id_movie";
-		$id = $_SESSION['id'];
-		$liste = mysqli_fetch_all(profile_list($db_connect,$select,$table,$column,$id), MYSQLI_NUM) or die ("Error can't connect to the database");
-		if(!empty($liste)){
-			echo "<tr>";
-			foreach($liste as $array){
-				foreach($array as $movie){
-					echo "<td>".$movie."</td></tr>";
-				}
-			};
-		}else{
-			echo "Your list is empty.<BR>";
-		}
-	}else if(isset($_REQUEST['movie_search'])){
-		$search = $_REQUEST['movie_search'];
-		$search = (string) $search;
-		$order = $_REQUEST['sort'];
-		require_once("../model/model_profile_search.php");
-		$liste = mysqli_fetch_all(movie_profil_search($db_connect,$search, $order), MYSQLI_NUM);
-		if(!empty($liste)){
-			echo "<tr>";
-			foreach($liste as $array){
-				$compt = 0;
-				foreach($array as $movie){
-					echo "<td>".$movie."</td>";
-				}
-			echo "</tr>";
+if(!empty($_REQUEST['movie_search'])){
+	$search = $_REQUEST['movie_search'];
+	$search = (string) $search;
+	$order = $_REQUEST['sort'];
+	require_once("../model/model_profile_search.php");
+	$liste = mysqli_fetch_all(movie_profil_search($db_connect,$search, $order), MYSQLI_NUM);
+	if(!empty($liste)){
+		echo "<tr>";
+		foreach($liste as $array){
+			$compt = 0;
+			foreach($array as $movie){
+				echo "<td>".$movie."</td>";
 			}
 		}else{
 			print("Not found");
