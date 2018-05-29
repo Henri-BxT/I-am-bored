ff3fd1f1b8b2046c5f4929b83facf525e68bd4ea
Joran-FreaZy <38242052+Joran-FreaZy@users.noreply.github.com>
2018-05-29 22:40:22 +0200

    Début dd'amélioration de précision sur la requête profile search

--- controller/controller_profile_movies.php
+++ controller/controller_profile_movies.php
@@ -1,13 +1,31 @@
 <?php
 session_start();
+
 require_once("../view/view_profile_list.html");
+
 $db_connect = mysqli_connect("localhost", "root", "", "im_bored") or die ("Error can't connect to the database");
-if(!empty($_REQUEST['movie_search'])){
+
+if((!empty($_REQUEST['movie_search'])) || (isset($_REQUEST['movie_search']))){
 	$search = $_REQUEST['movie_search'];
 	$search = (string) $search;
-	$order = $_REQUEST['sort'];
+	if(!isset($_REQUEST['name_grade'])){
+		$name_grade = "";
+	}
+	else{
+		$name_grade = $_REQUEST['name_grade'];
+	}
+	if(!isset($_REQUEST['asc_desc'])){
+		$asc_desc = "";
+	}
+	else{
+		$asc_desc = $_REQUEST['asc_desc'];
+	}
+	$type = $_REQUEST['type'];
+	
 	require_once("../model/model_profile_search.php");
-	$liste = mysqli_fetch_all(movie_profil_search($db_connect,$search, $order), MYSQLI_NUM);
+	
+	$liste = mysqli_fetch_all(movie_profil_search($db_connect,$search), MYSQLI_NUM);
+	
 	if(!empty($liste)){
 		echo "<tr>";
 		foreach($liste as $array){
@@ -19,13 +37,15 @@ if(!empty($_REQUEST['movie_search'])){
 		}else{
 			print("Not found");
 		}
-	}else {
+	}else{
 		require("../model/model_profile_list.php");
 		$select = "title";
 		$table = "MOVIES";
 		$column = "id_movie";
 		$id = $_SESSION['id'];
+		
 		$liste = mysqli_fetch_all(profile_list($db_connect,$select,$table,$column,$id), MYSQLI_NUM);
+		
 		if(!empty($liste)){
 			echo "<tr>";
 			foreach($liste as $array){
@@ -38,6 +58,7 @@ if(!empty($_REQUEST['movie_search'])){
 			echo "Your list is empty.<BR>";
 		}
 	}
+	
 mysqli_close($db_connect);
 echo "</TABLE></CENTER><BR><a href='controller_profil.php'>Retour</a></BODY></HTML>";
 ?>
--- model/model_profile_search.php
+++ model/model_profile_search.php
@@ -1,19 +1,30 @@
 <?php
 
-function movie_profil_search($db_connect, $search, $order){
+function type_movie_search(){
 	
-	if($order == "ASC" || "DESC"){
-		$column = "movies.title";
-	}if($order == "type"){
-		$column = "Types.name";
-		$order = "";
-	}if($order == "old"){
-		$column = "movies.date_diffusion";
-		$order = "ASC";
-	}if($order == "recent"){
-		$column = "movies.date_diffusion";
-		$order = "DESC";
-	}
+	$db_connect = mysqli_connect("localhost", "root", "", "im_bored") or die ("Error can't connect to the database");
+	
+	$login = $_SESSION['id'];
+	
+	$SQL = 'SELECT id_member FROM members WHERE login = "'.$login.'"';
+	$REQ = mysqli_query($db_connect, $SQL);
+	
+	$tmp = mysqli_fetch_array($REQ, MYSQLI_NUM);
+	
+	$SQL = 'SELECT DISTINCT Types.name
+	FROM listed_movies 
+	JOIN members ON listed_movies.id_member = members.id_member 
+	JOIN movies ON listed_movies.id_movie = movies.id_movie 
+	JOIN movie_types ON movie_types.id_movie = movies.id_movie
+	JOIN types ON movie_types.id_type = types.id_type
+	WHERE  members.id_member = "'.$tmp[0].'"
+	ORDER BY Types.name ASC';
+	
+	$REQ = mysqli_query($db_connect, $SQL);
+	return $REQ;
+}
+
+function movie_profil_search($db_connect, $search, $order){
 	
 	$login = $_SESSION['id'];
 	
