d8065c48719f1d10bcf279fab061ffefc8b5578d
WeabLord <38242410+WeabLord@users.noreply.github.com>
2018-05-30 16:22:38 +0200

    optimisation

--- model/model_profile_search.php
+++ model/model_profile_search.php
@@ -1,6 +1,6 @@
 <?php
 
-function type_movie_search(){
+function type_movie_search($media){
 	
 	$db_connect = mysqli_connect("localhost", "root", "", "im_bored") or die ("Error can't connect to the database");
 	
@@ -12,11 +12,11 @@ function type_movie_search(){
 	$tmp = mysqli_fetch_array($REQ, MYSQLI_NUM);
 	
 	$SQL = 'SELECT DISTINCT Types.name
-	FROM listed_movies 
-	JOIN members ON listed_movies.id_member = members.id_member 
-	JOIN movies ON listed_movies.id_movie = movies.id_movie 
-	JOIN movie_types ON movie_types.id_movie = movies.id_movie
-	JOIN types ON movie_types.id_type = types.id_type
+	FROM listed_'.$media.'s 
+	JOIN members ON listed_'.$media.'s.id_member = members.id_member 
+	JOIN '.$media.'s ON listed_'.$media.'s.id_'.$media.' = '.$media.'s.id_'.$media.'
+	JOIN '.$media.'_types ON '.$media.'_types.id_'.$media.' = '.$media.'s.id_'.$media.'
+	JOIN types ON '.$media.'_types.id_type = types.id_type
 	WHERE  members.id_member = "'.$tmp[0].'"
 	ORDER BY Types.name ASC';
 	
@@ -24,12 +24,12 @@ function type_movie_search(){
 	return $REQ;
 }
 
-function movie_profil_search($db_connect, $search, $asc_desc, $type, $name_grade){
+function movie_profil_search($db_connect, $search, $asc_desc, $type, $name_grade, $media){
 	
 	if(empty($name_grade)){
-		$name_grade = "listed_movies.favorit DESC, ";
+		$name_grade = "listed_".$media."s.favorit DESC, ";
 	}if(empty($asc_desc)){
-		$asc_desc = "movies.title ASC";
+		$asc_desc = $media."s.title ASC";
 	}if(empty($type)){
 		$types_name = "";
 	}else{
@@ -43,13 +43,13 @@ function movie_profil_search($db_connect, $search, $asc_desc, $type, $name_grade
 
 	$tmp = mysqli_fetch_array($REQ, MYSQLI_NUM);
 
-	$SQL = 'SELECT DISTINCT movies.image, movies.title, listed_movies.grade
-	FROM listed_movies 
-	JOIN members ON listed_movies.id_member = members.id_member 
-	JOIN movies ON listed_movies.id_movie = movies.id_movie 
-	JOIN movie_types ON movie_types.id_movie = movies.id_movie
-	JOIN types ON movie_types.id_type = types.id_type
-	WHERE movies.title LIKE "'.$search.'%"
+	$SQL = 'SELECT DISTINCT '.$media.'s.image, '.$media.'.title, listed_'.$media.'s.grade
+	FROM listed_'.$media.'s 
+	JOIN members ON listed_'.$media.'s.id_member = members.id_member 
+	JOIN '.$media.' ON listed_'.$media.'s.id_'.$media.' = '.$media.'s.id_'.$media.'
+	JOIN '.$media.'_types ON '.$media.'_types.id_'.$media.' = '.$media.'s.id_'.$media.'
+	JOIN types ON '.$media.'_types.id_type = types.id_type
+	WHERE '.$media.'s.title LIKE "'.$search.'%"
 	AND members.id_member = "'.$tmp[0].'" 
 	'.$types_name.' "'.$type.'"
 	ORDER BY '.$name_grade.' '.$asc_desc.' ';
@@ -58,37 +58,4 @@ function movie_profil_search($db_connect, $search, $asc_desc, $type, $name_grade
 	$REQ = mysqli_query($db_connect, $SQL);
 	return $REQ;
 }
-
-/*
-function music_profil_search($login, $search){
-
-	$host = "localhost";
-	$user = "root";
-	$bdd = "im_bored";
-	$password = "";
-
-	$db_connexion = mysqli_connect($host, $user, $password, $bdd) or die ("Error can't connect to the database");
-	
-	$SQL = 'SELECT id_member FROM members WHERE login = "'.$login.'"';
-	$REQ = mysqli_query($db_connexion, $SQL);
-
-	$tmp = mysqli_fetch_array($REQ, MYSQLI_NUM);
-
-	mysqli_free_result($REQ);
-	
-	$SQL = 'SELECT listed_musics.id_music, musics.title, musics.image, listed_musics.grade 
-			FROM listed_musics 
-			JOIN members ON listed_musics.id_member = members.id_member 
-			JOIN musics ON listed_musics.id_music = musics.id_music 
-			WHERE musics.title LIKE $search% 
-			AND members.id_member = $tmp[0]';
-			
-	$REQ = mysqli_query($db_connexion, $SQL);
-
-	$musics = mysqli_fetch_array($REQ, MYSQLI_NUM);
-
-	mysqli_free_result($REQ);
-	return $musics;
-}
-*/
 ?>
