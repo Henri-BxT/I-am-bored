b2c0321252e29067bf0ff1dade8de1ac8a89d627
SoluX1 <33317508+SoluX1@users.noreply.github.com>
2018-06-01 16:37:32 +0200

    Debug

--- model/model_quick_search.php
+++ model/model_quick_search.php
@@ -4,7 +4,7 @@ function quick_search($search, $format){
 	$db_connect = mysqli_connect("localhost", "root", "", "im_bored") or die ("Error can't connect to the database");
 
 	//Fetching the search
-	$sql = 'SELECT t1.image, t1.title, t1.id_'.$format.' 
+	$sql = 'SELECT DISTINCT t1.image, t1.title, t1.id_'.$format.' 
 			FROM '.$format.'s t1 
 			INNER JOIN listed_'.$format.'s t2 ON t1.id_'.$format.' = t2.id_'.$format.' 
 			WHERE title LIKE "'.$search.'%"
