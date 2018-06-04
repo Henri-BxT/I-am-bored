47051371a6601583959b687c0cfce346bb48b8b3
BxT <Henri-BxT@users.noreply.github.com>
2018-06-04 11:33:10 +0200

    fix

--- model/model_quick_search.php
+++ model/model_quick_search.php
@@ -1,18 +1,44 @@
 <?php
-
-function quick_search($search, $format){
+function type_movie_search($media){
+	
 	$db_connect = mysqli_connect("localhost", "root", "", "im_bored") or die ("Error can't connect to the database");
+	$REQ = mysqli_query($db_connect, $SQL);
+	
+	$tmp = mysqli_fetch_array($REQ, MYSQLI_NUM);
+	$SQL = 'SELECT DISTINCT t4.name
+		FROM listed_'.$media.'s AS t1
+		INNER JOIN '.$media.'s AS t2 ON t1.id_'.$media.' = t2.id_'.$media.'
+		INNER JOIN '.$media.'_types AS t3 ON t3.id_'.$media.' = t2.id_'.$media.'
+		INNER JOIN types AS t4 ON t3.id_type = t4.id_type
+		ORDER BY t4.name ASC';
+	
+	$REQ = mysqli_query($db_connect, $SQL);
+	return $REQ;
+}
 
+function quick_search($db_connect, $search, $asc_desc, $type, $name_grade, $media){	
+	$req = "listed_".$media."s.favorit DESC, ".$name_grade." ".$asc_desc;
+	if(empty($type)){
+		$types_name = "";
+	}else{
+		$types_name = "AND Types.name = ";
+	}
 	//Fetching the search
-	$sql = 'SELECT DISTINCT t1.image, t1.title, t1.id_'.$format.' 
-			FROM '.$format.'s t1 
-			INNER JOIN listed_'.$format.'s t2 ON t1.id_'.$format.' = t2.id_'.$format.' 
-			WHERE title LIKE "'.$search.'%"
-			ORDER BY t1.title ASC';
-		
-	$req = mysqli_query($db_connect, $sql);
+	$SQL = 'SELECT id_member FROM members WHERE login = "'.$login.'"';
+	$REQ = mysqli_query($db_connect, $SQL);
 
-	return $req;
+	$tmp = mysqli_fetch_array($REQ, MYSQLI_NUM);
+	$SQL = 'SELECT DISTINCT t1.image, t1.title, t2.grade
+		FROM '.$media.'s AS t1
+		INNER JOIN listed_'.$media.'s AS t2 ON t1.id_'.$media.' = t2.id_'.$media.'
+		INNER JOIN '.$media.'_types AS t3 ON t3.id_'.$media.' = t1.id_'.$media.'
+		INNER JOIN types AS t4 ON t3.id_type = t4.id_type
+		WHERE t1.title LIKE "'.$search.'%"
+		'.$types_name.' "'.$type.'"
+		ORDER BY '.$req;
+		
+	$REQ = mysqli_query($db_connect, $SQL);
+	return $REQ;
 }	
 ?>
 	
