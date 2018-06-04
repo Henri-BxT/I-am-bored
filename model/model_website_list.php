2f838c65ff73003d351587ce510b391e06293c7b
BxT <Henri-BxT@users.noreply.github.com>
2018-06-04 13:58:10 +0200

    Update model_website_list.php

--- model/model_website_list.php
+++ model/model_website_list.php
@@ -2,7 +2,7 @@
 function website_list($db_connect,$select,$table,$column){
     $list =  mysqli_query($db_connect,"SELECT $select
                                         FROM $table AS t1
-                                        INNER JOIN LISTED_$table AS t2 ON t2.$column = t2.$column
+                                        INNER JOIN LISTED_$table AS t2 ON t1.$column = t2.$column
                                         ORDER BY t1.title ASC;");
     return $list;
 }
