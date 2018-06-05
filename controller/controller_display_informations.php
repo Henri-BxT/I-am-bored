76d0f131829319b441b69a40be1a2aa92811481d
Joran-FreaZy <38242052+Joran-FreaZy@users.noreply.github.com>
2018-06-05 12:43:53 +0200

    Ajouts des images pour le système de rating

--- controller/controller_display_informations.php
+++ controller/controller_display_informations.php
@@ -7,17 +7,17 @@ require("../model/model_display_informations.php");
 $data = mysqli_fetch_all(display_info($db_connect,$_REQUEST['media'],$_REQUEST['id']), MYSQLI_NUM);
 if(!empty($data)){
     foreach($data as $infos){
-        echo "<img src=".$infos[2]." width='200px' height='200px'><br>"." Title : ".$infos[0]."<br>Date : ".$infos[1]."<br>Synopsis : ".$infos[3]."<p>";
-    }
+        echo "<center><img src=".$infos[2]." width='200px' height='200px'><br>"." Title : ".$infos[0]."<br>Date : ".$infos[1]."<br>Synopsis : ".$infos[3]."<p></center>";
+   }
     require("../model/model_registration_id_search.php");
     $id_member = mysqli_fetch_array(id_search($db_connect, $_SESSION['id']), MYSQLI_NUM);
     $data = mysqli_fetch_array(search_list($db_connect,$_REQUEST['media'],$_REQUEST['id'],$id_member[0]), MYSQLI_NUM);
     if(!empty($data)){
         $data = mysqli_fetch_array(search_favorit($db_connect,$_REQUEST['media'],$_REQUEST['id'],$id_member[0]), MYSQLI_NUM);
         if(!empty($data[0])){
-            echo "Déjà dans vos Favoris !";
+            echo "<center>Déjà dans vos Favoris !<center>";
         }else{
-            echo "Déjà dans votre liste !";
+            echo "<center>Déjà dans votre liste !<center>";
         }
     }
 }else{
