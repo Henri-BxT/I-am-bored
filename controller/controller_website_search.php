2cc099c31a1bbbc6358f86e025321e81c06c25c9
SoluX1 <33317508+SoluX1@users.noreply.github.com>
2018-06-19 14:23:15 +0200

    tentative

--- controller/controller_website_search.php
+++ controller/controller_website_search.php
@@ -53,16 +53,14 @@ if(!empty($_REQUEST['search'])){
 					$array[3] = "Pas de note";
 				}	
 				echo "<td><a href='controller_display_informations.php?id=".$array[2]."&media=".$_GET['media']."'><img src=".$array[0]." width='100' height='100px'></a></td><td><a href='controller_display_informations.php?id=".$array[2]."&media=".$_GET['media']."'>".$array[1]."</a></td><td>";
-				for($i = 1; $i <= $array[3]; $i++) {
-					echo"<img id = '".$i."' src='../ressources/icons/full_star.png' width='20px' height='20px' data-grade = '".$array[3]."'>";
-				}
-				if($array[3]<6){
-					$empty_star = 5 - $array[3];
-					for($i=1; $i <= $empty_star; $i++){
-						echo"<img id = '".$i."' src='../ressources/icons/empty_star.png' width='20px' height='20px' data-grade = '".$array[3]."'>";
+				for($i=0;$i<5;$i++){
+					if($i<=$array[3]){
+						echo"<img src='../ressources/icons/full_star.png' width='20px' height='20px'>";
+					}else{
+						echo"<img src='../ressources/icons/empty_star.png' width='20px' height='20px'>";
 					}
 				}
-				echo "</td></tr>";	
+					echo "</td></tr>";	
         	}
         	echo "</center></table>";
     	}else{
