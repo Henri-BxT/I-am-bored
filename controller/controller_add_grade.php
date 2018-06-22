9ca9e2802d608abcc1b1bd62017b240ad38ff21a
WeabLord <38242410+WeabLord@users.noreply.github.com>
2018-06-22 10:25:08 +0200

    up-grade

--- controller/controller_add_grade.php
+++ controller/controller_add_grade.php
@@ -1,5 +1,10 @@
 <?php
+session_start();
 require("../model/model_add_rate.php");
+require("../model/model_manage_list.php");
+if(verif_l($_GET["media"], $_SESSION["id"], $_GET["id"]) == false) {
+	add_l($_GET["media"], $_SESSION["id"], $_GET["id"]);
+}
 add_grade_movie($_GET["media"], $_GET["grade"],$_GET["id"]);
 header("Location: controller_display_informations.php?grade=".$_GET["grade"]."&media=".$_GET["media"]."&id=".$_GET['id']."");
 ?>
